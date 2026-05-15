a5e90641f0602d6302b13452b71a3292ff1850d3
Drew Yang <dyang@arize.com>
2026-05-14 19:02:19 -0500

    one dark theme

---
+++ lua/custom/plugins/onedark.lua
@@ -0,0 +1,12 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  "navarasu/onedark.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('onedark').setup {
+      style = 'cool'
+    }
+    require('onedark').load()
+  end
+}
